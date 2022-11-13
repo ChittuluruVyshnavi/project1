b8751e6fb80e172d68212e8ee5cf45cd804fa0f2
ChittuluruVyshnavi <103238003+ChittuluruVyshnavi@users.noreply.github.com>
2022-11-13 16:32:07 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,12 @@
+package helloworld.program.sampleprogram;
+
+public class Project {
+
+	public static void main(String[] args) 
+    {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
